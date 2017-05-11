9018d6a858e568ed32a87d8b41abb19d975d5ec6
Jeffrey Hu <hujh@mit.edu>
2017-05-11 16:33:04 -0400

    Fixed none bug

--- maskToPolygons.py
+++ maskToPolygons.py
@@ -19,21 +19,22 @@ class MaskToPolygons:
             mask = mask.astype(np.uint8)
 
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            contours = [c for c,h in zip(contours, hierarchy[0]) if cv2.contourArea(c) > 100 and h[3] < 0]
+            if contours:
+                contours = [c for c,h in zip(contours, hierarchy[0]) if cv2.contourArea(c) > 100 and h[3] < 0]
 
-            # Visualize
-            for c in contours:
-                color = np.random.randint(0, 255, size=3)
-                cv2.drawContours(debug, [c], -1, (color[0], color[1], color[2]), 3)
+                # Visualize
+                for c in contours:
+                    color = np.random.randint(0, 255, size=3)
+                    cv2.drawContours(debug, [c], -1, (color[0], color[1], color[2]), 3)
 
-            polygons = []
-            for c in contours:
-                n = c.shape[0]
-                c = c.reshape((n,2))
-                polygon = c.tolist()
-                polygons.append(polygon)
+                polygons = []
+                for c in contours:
+                    n = c.shape[0]
+                    c = c.reshape((n,2))
+                    polygon = c.tolist()
+                    polygons.append(polygon)
 
-            categoryToPolygons[category] = polygons
+                categoryToPolygons[category] = polygons
 
         return categoryToPolygons, debug
 
@@ -50,10 +51,10 @@ class MaskToPolygons:
 
 if __name__=="__main__":
     name = sys.argv[1]
-    image = cv2.imread(name, 0)
+    mask = cv2.imread(name, 0)
 
     converter = MaskToPolygons()
-    categoryToPolygons, debug = converter.processMask(image)
+    categoryToPolygons, debug = converter.processMask(mask)
     plt.imshow(debug)
     plt.show()
 
