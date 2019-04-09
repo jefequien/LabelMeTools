c5874b2bfd361400da8f034556216be4eb505f84
Jeffrey Hu <jeffreyhu@Jeffreys-MacBook-Pro-2.local>
2019-04-09 02:41:35 -0400

    filter

--- src/filter_coco.py
+++ src/filter_coco.py
@@ -4,6 +4,9 @@ import argparse
 from coco_utils.coco_format import *
 from coco_utils.dummy_datasets import *
 
+def read_list(file_name):
+    with open(file_name, 'r') as f:
+        return f.read().splitlines()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,8 +15,6 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--out_fn', type=str, default="../coco.json", help='Output coco file')
 
     parser.add_argument('-i', '--im_list', type=str, help='List of images')
-    parser.add_argument('-a', '--ann_list', type=str, help='List of annotations. Typically the output of maskrcnn')
-    parser.add_argument('-c', '--cat_list', type=str, help='List of categories')
     args = parser.parse_args()
     print(args)
 
@@ -29,13 +30,17 @@ if __name__ == "__main__":
 
     # Filter by image list
     if args.im_list:
-        filtered = []
+        filtered_images = []
+        filtered_annotations = []
         im_list = set(read_list(args.im_list))
         for img in images:
             if img["file_name"] in im_list:
-                filtered.append(img)
-        images = filtered
+                annIds = coco.getAnnIds(imgIds=[img["id"]])
+                anns = coco.loadAnns(annIds)
+                filtered_images.append(img)
+                filtered_annotations.extend(anns)
+        images = filtered_images
+        annotations = filtered_annotations
 
     save_ann_fn(images, annotations, categories, args.out_fn)
     print_ann_fn(args.out_fn)
-    
